7885ac6c51cdb77528c648d2d5f158e009833a04
Akshita Bhardwaj <bhar.akshita28@gmail.com>
2026-09-21 00:42:03 +0530

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        if(n==1 || n==7) return true;
+        if(n<10){
+            return false;
+        } 
+        int dig=0;
+        while(n>0){
+            dig+=pow(n%10,2);
+            n=n/10;
+        }
+        return isHappy(dig);
+    }
+};
